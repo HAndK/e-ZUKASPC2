4106e599a36ecf9f7478dac95e125a322d6e5b8d
Wataaki <poteto94@outlook.jp>
2014-11-14 02:34:50 +0900

    カラーパレット(未完)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var stage, label, shape, oldPt, oldMidPt, size, color;
+var stage, label, shape, oldPt, oldMidPt, size, color, actBtn;
 // ?pict=画像名 取得
 var picpath = "img/" + location.search.split("=")[1] + ".jpg";
 
@@ -6,8 +6,9 @@ function init() {
   // canvasの設定
   stage = new createjs.Stage("demoCanvas");
   stage.enableDOMEvents(true);
-  stage.canvas.height = window.innerHeight - 100;
-  stage.canvas.width = window.innerWidth;
+  stage.canvas.height = window.innerHeight - 250;
+  stage.canvas.width = window.innerWidth - 50;
+  createjs.Touch.enable(stage);
 
   // ぬりえ画像
   backcontainer = new createjs.Container();
@@ -20,29 +21,40 @@ function init() {
 
   stage.addEventListener("stagemousedown", mouseDown);
   stage.addEventListener("stagemouseup", mouseUp);
-  $('.btn').on("click", getColor);
+  $('.btn').on("click", btnClick);
 
   stage.update();
 }
 
-function mouseDown(evt) {
+/**
+ * [mouseDown description]
+ * @return {[type]} [description]
+ */
+function mouseDown() {
   size = $("#setSize").val();
   oldPt = new createjs.Point(stage.mouseX, stage.mouseY);
   oldMidPt = oldPt;
   stage.addEventListener("stagemousemove", mouseMove);
 }
 
-function mouseUp(evt) {
+/**
+ * [mouseUp description]
+ * @return {[type]} [description]
+ */
+function mouseUp() {
   stage.removeEventListener("stagemousemove", mouseMove);
 }
 
-function mouseMove(evt) {
+/**
+ * [mouseMove description]
+ * @return {[type]} [description]
+ */
+function mouseMove() {
   var midPt = new createjs.Point(oldPt.x + stage.mouseX>>1, oldPt.y+stage.mouseY>>1);
   shape.graphics.beginStroke(color)
        .setStrokeStyle(size, "round", "round")
        .moveTo(midPt.x, midPt.y)
        .curveTo(oldPt.x, oldPt.y, oldMidPt.x, oldMidPt.y);
-
   oldPt.x = stage.mouseX;
   oldPt.y = stage.mouseY;
   oldMidPt.x = midPt.x;
@@ -51,7 +63,14 @@ function mouseMove(evt) {
   stage.update();
 }
 
-// 色選択
-function getColor() {
+// 色選択, ボタンの押された風な動き
+function btnClick() {
+  // 同じボタンを連続で押させない
+  if($(this).get(0)==$(actBtn).get(0)) return;
   color = $(this).css("background-color");
+  $(this).css({'box-shadow':'none', 'transform':'translateY(5px)'});
+  if(actBtn) {
+    $(actBtn).css({'box-shadow':'0 5px 0 #0e8c73', 'transform':'translateY(0)'});
+  }
+  actBtn = $(this);
 }
